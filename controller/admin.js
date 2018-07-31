a01fc4fdf6613a8ea7e85c1927350761231d2270
bookyo <195996048@qq.com>
2018-07-31 10:21:07 +0800

    fix delete

--- controller/admin.js
+++ controller/admin.js
@@ -150,7 +150,8 @@ exports.delete = function(req,res) {
                     if (exists) {
                         fs.unlinkSync(movie.path);
                     }
-                })
+                });
+                deleteall("./public/videos/"+id);
                 res.json({success:1});
             })
          });
@@ -389,3 +390,19 @@ exports.ruku = function(req, res) {
         res.json({success: 1});
     });
 }
+
+function deleteall(path) {
+    var files = [];
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        files.forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.statSync(curPath).isDirectory()) { // recurse
+                deleteall(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
